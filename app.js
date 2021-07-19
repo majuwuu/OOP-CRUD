7ed8ef99b0d4885dbafb17f133c715c9102f8edb
Jere <agustinj.gauna@hotmail.com>
2021-07-19 20:14:46 -0300

    dinamic product list

--- app.js
+++ app.js
@@ -9,7 +9,17 @@ class Product {
 class UI {
   addProduct(product){
     const productList= document.getElementById('product-list');
-
+    const element = document.createElement('div');
+    element.innerHTML=  `
+        <div class="card text-center mb-4"> 
+           <div class="card-body">
+           <strong>Product Name</strong>: ${product.name}
+           <strong>Product Price</strong>:${product.price}
+           <strong>Product Year</strong>: ${product.year}   
+          </div>
+        </div>    
+    `;
+      productList.appendChild(element);
   }
 
   deleteProduct(){
@@ -24,12 +34,14 @@ class UI {
 //DOM Events//
 
 document.getElementById('product-form')
-.addEventListener('submit', function(e) {
+.addEventListener('submit', function (e) {
   const  name = document.getElementById('name').value;
   const price = document.getElementById('price').value;
   const year = document.getElementById('year').value;
 
   const product = new Product(name, price, year);
+  const ui = new UI();
+  ui.addProduct(product)
 
   e.preventDefault();
 })
